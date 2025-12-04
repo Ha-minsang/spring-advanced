2cb266dd399b72595902ad0736a87ab372f27ec2
Ha-minsang <gkalstkd@gmail.com>
2025-12-04 19:44:46 +0900

    Lv.7 패스워드 인코더 테스트 코드 추가

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -1,5 +1,7 @@
 package org.example.expert.config;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,7 +16,22 @@ class PasswordEncoderTest {
     private PasswordEncoder passwordEncoder;
 
     @Test
-    void matches_메서드가_정상적으로_동작한다() {
+    @DisplayName("encode 메서드가 정상적으로 작동한다")
+    void encode_ReturnsEncodedPassword() {
+        // given
+        String rawPassword = "testPassword";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        // when
+        BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
+
+        // then
+        assertTrue(result.verified);
+    }
+
+    @Test
+    @DisplayName("matches 메서드가 정상적으로 작동한다")
+    void matches_ShouldWorkAsExpected() {
         // given
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
